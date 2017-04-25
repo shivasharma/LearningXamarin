bf9174bc2519df54e05c1590c6dca0b33b6e7c91
Shiva Sharma <shivathebravo@gmail.com>
2017-04-24 23:27:19 -0400

    Change fontsize

--- LearningXamarinProject/LearningXamarinProject/MainPage.xaml.cs
+++ LearningXamarinProject/LearningXamarinProject/MainPage.xaml.cs
@@ -9,9 +9,28 @@ namespace LearningXamarinProject
 {
     public partial class MainPage : ContentPage
     {
+
+        private int _index = 0;
+        private string[] _quotes = new string[]
+        {
+            "Life is like riding a bicycle. To keep your balance, you must keep moving.",
+            "You can't blame gravity for falling in love.",
+            "Look deep into nature, and then you will understand everything better."
+        };
         public MainPage()
         {
             InitializeComponent();
+            currentQuote.Text = _quotes[_index];
         }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            _index++;
+            if (_index >= _quotes.Length)
+                _index = 0;
+
+            currentQuote.Text = _quotes[_index];
+        }
+        
     }
 }
